2ef7c5746bbd5d173e6bdf4161a3820eb3244d01
Tynor Fujimoto <tynorf@gmail.com>
2019-08-05 12:03:59 -0700

    Add linting and formatting

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+import * as http2 from 'http2';
+
+export interface SecretInfo {
+  readonly wrapping_key: string;
+}
+
+export interface SecretInfoStorage {
+  getSecretInfo(name: string): Promise<SecretInfo>;
+  setSecretInfo(name: string, info: SecretInfo): Promise<void>;
+  getAllSecretInfo(): Promise<{[name: string]: SecretInfo}>;
+}
+
+export interface Settings {
+  readonly hostname: string;
+  readonly options: http2.SecureClientSessionOptions;
+}
